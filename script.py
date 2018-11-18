724318eed175c77f1110e9ed63ef841c379b32b7
Juhis <juhis.ni@gmail.com>
2018-11-18 11:42:12 +0200

    Add test objects

---
+++ script.py
@@ -0,0 +1,4 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shangai, China', ['historical site', 'art']]
+
